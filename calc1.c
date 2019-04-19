4af11d43aabf70f970f86c5263ad898b38a45dfd
Saurabh702 <bhandarisaurabh800@gmail.com>
2019-04-19 22:09:36 +0530

    Indentation

--- calc1.c
+++ calc1.c
@@ -6,12 +6,12 @@
 
 jmp_buf resume_here;
 
-
-typedef struct String 
+typedef struct String
 {
-	char *ptr;
-	size_t len;
-}string;
+    char *ptr;
+    size_t len;
+} string;
+
 void init_string(string *,int);
 void display_string(string);
 
@@ -25,30 +25,30 @@ void init_string(string *s,int len)
         fprintf(stderr, "malloc() failed\n");
         exit(EXIT_FAILURE);
     }
-	
-	if(len > 0)
-		s->ptr[len] = '\0';
-	else
-		s->ptr[0] = '\0';
+
+    if(len > 0)
+        s->ptr[len] = '\0';
+    else
+        s->ptr[0] = '\0';
 }
+
 void display_string(string s)
 {
     printf("%s",s.ptr);
 }
 
-
 typedef struct Token
 {
-	char *type;
-	string value;
-}Token;
+    char *type;
+    string value;
+} Token;
 
 typedef struct Interpreter
 {
-	char *text;
-	int pos;
-	Token current_token;
-}Interpreter;
+    char *text;
+    int pos;
+    Token current_token;
+} Interpreter;
 
 void interpret(Interpreter *);
 Token get_next_token(Interpreter *);
@@ -57,68 +57,72 @@ void parse_error();
 
 void interpret(Interpreter *i)
 {
-	Token left,op,right;
-	int result;
+    Token left,op,right;
+    int result;
 
-	i->pos = 0;
-	i->current_token.type = "";
+    i->pos = 0;
+    i->current_token.type = "";
     init_string(&i->current_token.value,0);
-	
+
     i->current_token = get_next_token(i);
     //printf("%s %d %s %s\n",i->text,i->pos,i->current_token.type,i->current_token.value.ptr);
     left = i->current_token;
     parse(i,"INTEGER");
-	int flag = 0;
+    int flag = 0;
     op = i->current_token;
-	
+
     if(strcmp(op.type,"PLUS") == 0)
-	{
-		parse(i,"PLUS");
-		flag = 1;
-	}
-	else if(strcmp(op.type,"MINUS") == 0)
-	{
-		parse(i,"MINUS");
-		flag = 2;
-	}
-	else if(strcmp(op.type,"MULTIPLY") == 0)
-	{
-		parse(i,"MULTIPLY");
-		flag = 3;
-	}
-	else if(strcmp(op.type,"DIVIDE") == 0)
-	{
-		parse(i,"DIVIDE");
-		flag = 4;
-	}
-	else
-		parse_error();
-	
+    {
+        parse(i,"PLUS");
+        flag = 1;
+    }
+    else if(strcmp(op.type,"MINUS") == 0)
+    {
+        parse(i,"MINUS");
+        flag = 2;
+    }
+    else if(strcmp(op.type,"MULTIPLY") == 0)
+    {
+        parse(i,"MULTIPLY");
+        flag = 3;
+    }
+    else if(strcmp(op.type,"DIVIDE") == 0)
+    {
+        parse(i,"DIVIDE");
+        flag = 4;
+    }
+    else
+        parse_error();
+
     right = i->current_token;
     parse(i,"INTEGER");
-	
+
     switch(flag)
-	{
-		case 1:	result = atoi(left.value.ptr) + atoi(right.value.ptr);
-				break;
-		case 2: result = atoi(left.value.ptr) - atoi(right.value.ptr);
-				break;
-		case 3: result = atoi(left.value.ptr) * atoi(right.value.ptr);
-				break;
-		case 4: result = atoi(left.value.ptr) / atoi(right.value.ptr);
-				break;
-	}
-	
+    {
+    case 1:
+        result = atoi(left.value.ptr) + atoi(right.value.ptr);
+        break;
+    case 2:
+        result = atoi(left.value.ptr) - atoi(right.value.ptr);
+        break;
+    case 3:
+        result = atoi(left.value.ptr) * atoi(right.value.ptr);
+        break;
+    case 4:
+        result = atoi(left.value.ptr) / atoi(right.value.ptr);
+        break;
+    }
+
     printf("%d\n",result);
 }
 
 void parse(Interpreter *i,char *type)
 {
-        //printf("%s %d %s %s\n",i->text,i->pos,i->current_token.type,i->current_token.value.ptr);
-		if(strcmp(i->current_token.type,type) == 0)
-			i->current_token = get_next_token(i);
-        else
-			parse_error();
+    //printf("%s %d %s %s\n",i->text,i->pos,i->current_token.type,i->current_token.value.ptr);
+    if(strcmp(i->current_token.type,type) == 0)
+        i->current_token = get_next_token(i);
+    else
+        parse_error();
 }
 
 void parse_error()
@@ -129,68 +133,81 @@ void parse_error()
 
 Token get_next_token(Interpreter *i)
 {
-	string current_char;
-	init_string(&current_char,1);
+    string current_char;
+    init_string(&current_char,1);
     if (i->pos > strlen(i->text) - 1)
-		return (Token){"EOF",current_char};
+        return (Token)
+    {"EOF",current_char
+    };
 
     current_char.ptr[0] = i->text[i->pos];
-	
-	while(isspace(current_char.ptr[0]))
-		current_char.ptr[0] = i->text[++i->pos];
-	
-	if(isdigit(current_char.ptr[0]))
-    {	int j = 0;
+
+    while(isspace(current_char.ptr[0]))
+        current_char.ptr[0] = i->text[++i->pos];
+
+    if(isdigit(current_char.ptr[0]))
+    {
+        int j = 0;
         while(isdigit(i->text[++i->pos]))
-			current_char.ptr[++j] = i->text[i->pos];
-		current_char.ptr[++j] = '\0';
-        return (Token){"INTEGER",current_char};
+            current_char.ptr[++j] = i->text[i->pos];
+        current_char.ptr[++j] = '\0';
+        return (Token)
+        {"INTEGER",current_char
+        };
     }
     else if(current_char.ptr[0] == '+')
     {
         i->pos++;
-        return (Token){"PLUS",current_char};
+        return (Token)
+        {"PLUS",current_char
+        };
     }
-	else if(current_char.ptr[0] == '-')
-	{
-		i->pos++;
-        return (Token){"MINUS",current_char};
-	}
-	else if(current_char.ptr[0] == '*')
-	{
-		i->pos++;
-        return (Token){"MULTIPLY",current_char};
-	}
-	else if(current_char.ptr[0] == '/')
-	{
-		i->pos++;
-        return (Token){"DIVIDE",current_char};
-	}
+    else if(current_char.ptr[0] == '-')
+    {
+        i->pos++;
+        return (Token)
+        {"MINUS",current_char
+        };
+    }
+    else if(current_char.ptr[0] == '*')
+    {
+        i->pos++;
+        return (Token)
+        {"MULTIPLY",current_char
+        };
+    }
+    else if(current_char.ptr[0] == '/')
+    {
+        i->pos++;
+        return (Token)
+        {"DIVIDE",current_char
+        };
+    }
     else
-		parse_error();
+        parse_error();
 }
 
 int main()
 {
-	//Turn off buffering in stdout
-	setvbuf(stdout, NULL, _IONBF, 0);
-	
-	Interpreter i;
+    //Turn off buffering in stdout
+    setvbuf(stdout, NULL, _IONBF, 0);
+
+    Interpreter i;
     i.text = malloc(sizeof(char*));
 
-	while(1)
-	{
-		setjmp(resume_here); //check
-		printf("calc> ");
-		//scanf("%s",i.text);  //for whitespaces use gets
-		gets(i.text);
-		if(strcmp(i.text,"exit") == 0)
-			break;
+    while(1)
+    {
+        setjmp(resume_here); //check
+        printf("calc> ");
+        //scanf("%s",i.text);  //for whitespaces use gets
+        gets(i.text);
+        if(strcmp(i.text,"exit") == 0)
+            break;
 
-		interpret(&i);
-	}
-	
-	free(i.text);
-	
-	return 0;
+        interpret(&i);
+    }
+
+    free(i.text);
+
+    return 0;
 }
